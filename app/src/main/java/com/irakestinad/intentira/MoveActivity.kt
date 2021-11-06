6d155eedb6d385358e39ccf384ac8a6ce8b90ce0
irakestinad <irakestinadamayanti1@gmail.com>
2021-11-06 18:18:48 +0700

    IntentIra
    Tugas Praktikum MainActivity

---
+++ app/src/main/java/com/irakestinad/intentira/MoveActivity.kt
@@ -0,0 +1,32 @@
+package com.irakestinad.intentira
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MoveActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnBackToMainPage: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+
+        btnBackToMainPage = findViewById(R.id.btn_backto_main_page)
+        btnBackToMainPage.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null)
+            when (v.id) {
+                R.id.btn_backto_main_page -> run {
+                    val intent = Intent(this, BackToMainPage::class.java)
+                    startActivity(intent)
+                }
+            }
+    }
+
+    class BackToMainPage {
+
+    }
+}
